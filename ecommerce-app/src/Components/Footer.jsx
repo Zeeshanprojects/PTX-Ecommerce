4aa68f580be667ad00bd19e7ad946b19ebbcef43
Zeeshan Khan <126192035+Zeeshanprojects@users.noreply.github.com>
2025-04-30 16:00:41 +0500

    Home page

--- ecommerce-app/src/Components/Footer.jsx
+++ ecommerce-app/src/Components/Footer.jsx
@@ -2,170 +2,163 @@ import React from 'react'
 
 export default function Footer() {
   return (
-<>
-<>
-  {/* Remove the container if you want to extend the Footer to full width. */}
-  <div className="container-fluid p-0">
-    {/* Footer */}
-    <footer
-     
-    >
-      {/* Section: Social media */}
-      <section
-       
-      >
-        {/* Left */}
-        <div className="me-5">
-                </div>
-        {/* Left */}
-        {/* Right */}
-        <div>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-facebook-f" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-twitter" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-google" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-instagram" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-linkedin" />
-          </a>
-          <a href="" className="text-white me-4">
-            <i className="fab fa-github" />
-          </a>
-        </div>
-        {/* Right */}
-      </section>
-      {/* Section: Social media */}
-      {/* Section: Links  */}
-      <section className="">
-        <div className="container text-center text-md-start mt-5">
-          {/* Grid row */}
-          <div className="row mt-3">
-            {/* Grid column */}
-            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              {/* Content */}
-              <h6 className="text-uppercase fw-bold">Company name</h6>
-              <hr
-                className="mb-4 mt-0 d-inline-block mx-auto"
-                style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
-              />
-              <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
-              </p>
-            </div>
-            {/* Grid column */}
-            {/* Grid column */}
-            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              {/* Links */}
-              <h6 className="text-uppercase fw-bold">Products</h6>
-              <hr
-                className="mb-4 mt-0 d-inline-block mx-auto"
-                style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
-              />
-              <p>
-                <a href="#!" className="text-dark">
-                  MDBootstrap
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                  MDWordPress
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                  BrandFlow
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                  Bootstrap Angular
-                </a>
-              </p>
-            </div>
-            {/* Grid column */}
-            {/* Grid column */}
-            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              {/* Links */}
-              <h6 className="text-uppercase fw-bold">Useful links</h6>
-              <hr
-                className="mb-4 mt-0 d-inline-block mx-auto"
-                style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
-              />
-              <p>
-                <a href="#!" className="text-dark">
-                  Your Account
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                  Become an Affiliate
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                  Shipping Rates
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-dark">
-                  Help
-                </a>
-              </p>
-            </div>
-            {/* Grid column */}
-            {/* Grid column */}
-            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              {/* Links */}
-              <h6 className="text-uppercase fw-bold">Contact</h6>
-              <hr
-                className="mb-4 mt-0 d-inline-block mx-auto"
-                style={{ width: 60, backgroundColor: "#7c4dff", height: 2 }}
-              />
-              <p>
-                <i className="fas fa-home mr-3" /> New York, NY 10012, US
-              </p>
-              <p>
-                <i className="fas fa-envelope mr-3" /> info@example.com
-              </p>
-              <p>
-                <i className="fas fa-phone mr-3" /> + 01 234 567 88
-              </p>
-              <p>
-                <i className="fas fa-print mr-3" /> + 01 234 567 89
-              </p>
-            </div>
-            {/* Grid column */}
-          </div>
-          {/* Grid row */}
-        </div>
-      </section>
-      {/* Section: Links  */}
-      {/* Copyright */}
-      <div
-        className="text-center p-3"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-      >
-        © 2020 Copyright:
-        <a className="text-dark" href="https://mdbootstrap.com/">
-          MDBootstrap.com
-        </a>
-      </div>
-      {/* Copyright */}
-    </footer>
-    {/* Footer */}
-  </div>
-  {/* End of .container */}
-</>
 
+<>
+<div className="container-fluid p-0">
+      <footer
+        className="text-center text-lg-start text-dark"
+        style={{ backgroundColor: "#ECEFF1" }}
+      >
+        <section
+      
+        >
+          
+          <div>
+            <a href="#" className="text-white me-4">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" className="text-white me-4">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="#" className="text-white me-4">
+              <i className="fab fa-google"></i>
+            </a>
+            <a href="#" className="text-white me-4">
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a href="#" className="text-white me-4">
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a href="#" className="text-white me-4">
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+        </section>
+
+        <section>
+          <div className="container-fluid text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold">Company name</h6>
+                <hr
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{
+                    width: "60px",
+                    backgroundColor: "#7c4dff",
+                    height: "2px",
+                  }}
+                />
+                <p>
+                  Here you can use rows and columns to organize your footer
+                  content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                  elit.
+                </p>
+              </div>
+
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold">Products</h6>
+                <hr
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{
+                    width: "60px",
+                    backgroundColor: "#7c4dff",
+                    height: "2px",
+                  }}
+                />
+                <p>
+                  <a href="#!" className="text-dark">
+                    MDBootstrap
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-dark">
+                    MDWordPress
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-dark">
+                    BrandFlow
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-dark">
+                    Bootstrap Angular
+                  </a>
+                </p>
+              </div>
+
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold">Useful links</h6>
+                <hr
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{
+                    width: "60px",
+                    backgroundColor: "#7c4dff",
+                    height: "2px",
+                  }}
+                />
+                <p>
+                  <a href="#!" className="text-dark">
+                    Your Account
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-dark">
+                    Become an Affiliate
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-dark">
+                    Shipping Rates
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-dark">
+                    Help
+                  </a>
+                </p>
+              </div>
+
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <h6 className="text-uppercase fw-bold">Contact</h6>
+                <hr
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{
+                    width: "60px",
+                    backgroundColor: "#7c4dff",
+                    height: "2px",
+                  }}
+                />
+                <p>
+                  <i className="fas fa-home mr-3"></i> New York, NY 10012, US
+                </p>
+                <p>
+                  <i className="fas fa-envelope mr-3"></i> info@example.com
+                </p>
+                <p>
+                  <i className="fas fa-phone mr-3"></i> + 01 234 567 88
+                </p>
+                <p>
+                  <i className="fas fa-print mr-3"></i> + 01 234 567 89
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2020 Copyright:
+          <a className="text-dark" href="https://mdbootstrap.com/">
+            MDBootstrap.com
+          </a>
+        </div>
+      </footer>
+    </div>
+  
 </>
   )
+
 }
--- ecommerce-app/src/Pages/Home.jsx
+++ ecommerce-app/src/Pages/Home.jsx
@@ -2,137 +2,277 @@ import React from "react";
 import Image from "../Images/Image";
 import "./Home.css";
 export default function Home() {
+  const products = [
+    {
+      id: 1,
+      name: " Yellow",
+      price: "PKR 1,999",
+      image: Image.image1,
+   
+    },
+    {
+      id: 2,
+      name: "Red",
+      price: "PKR 2,299",
+      image: Image.image2,
+      
+    },
+    {
+      id: 3,
+      name: "Urban Black Shirt",
+      price: "PKR 2,799",
+      image: Image.image3,
+    },
+    {
+      id: 4,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image4,
+    },
+    {
+      id: 5,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image5,
+    },
+    {
+      id: 6,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image6,
+    },
+    {
+      id: 7,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image7,
+    },
+    {
+      id: 8,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image8,
+    },
+    {
+      id: 9,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image9,
+    },
+    {
+      id: 10,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image10,
+    },
+    {
+      id: 11,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image11,
+    },
+    {
+      id: 12,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image12,
+    },
+    {
+      id: 13,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image13,
+    },
+    {
+      id: 14,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image14,
+    },
+    {
+      id: 15,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image15,
+    },
+    {
+      id: 16,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image16,
+    },
+    {
+      id: 17,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image17,
+    },
+    {
+      id: 18,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image18,
+    },
+    {
+      id: 19,
+      name: "Ocean Blue Tee",
+      price: "PKR 2,499",
+      image: Image.image19,
+    },
+  ];
+
   return (
     <>
       <div className="image-wrapper">
         <img
-          src={Image.sliderimage2}
-          alt="slider-image2"
+          src={Image.banner}
+          alt="slider-image1"
           className="sliderimage"
         />
         <div className="imagetext">
-          <h6>Summer Collection</h6>
-          <h2>Fall - Summer Collections 2025</h2>
-          <p>
-            A specialist label creating luxury essentials Ethically crafted with
-            an <br /> unwavering commitment to exceptional quality
-          </p>
-          <button type="button" className="btn btn-dark ">
+          
+          <button type="button" className="btn btn-outline-light mt-4 ">
             SHOP NOW
           </button>
         </div>
       </div>
 
-      <br/>
+      <div className="space"></div>
       <h2 className="text-center">FEATURED PRODUCT</h2>
-      <div className="container">
-        <div className="row mt-5">
-          <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-            <div className="box">
+      <div className="space"></div>
+      <div className="container latest-products-section ">
+        <div className="row">
+          {/* Product 1 */}
+          <div className="col-sm-12 col-md-12 col-lg-4">
+            <div className="product-box">
               <img
-                src={Image.image1}
-                className="card-img-top"
-                alt="Tshirt1"
+                src={Image.product1}
+                className="product-image"
+                alt="PTX Essential"
               />
-              <div className="card-body">
+              <div className="product-info">
+                <h3 className="product-title">PTX Essential</h3>
+                <p className="product-description">
+                  Our wide range of quality premium essentials
+                </p>
               </div>
             </div>
           </div>
-          <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-            <div className="box">
+
+          {/* Product 2 */}
+          <div className="col-sm-12 col-md-12 col-lg-4 ">
+            <div className="product-box">
               <img
-                src={Image.image2}
-                className="card-img-top"
-                alt="Tshirt2"
+                src={Image.product2}
+                className="product-image"
+                alt="PTX Juniors"
               />
-              <div className="card-body">
-                
+              <div className="product-info">
+                <h3 className="product-title">PTX Juniors</h3>
+                <p className="product-description">
+                  Our new range of essentials for kids
+                </p>
               </div>
             </div>
           </div>
-          <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-            <div className="box">
+
+          {/* Product 3 */}
+          <div className="col-sm-12 col-md-12 col-lg-4">
+            <div className="product-box">
               <img
-                src={Image.image3}
-                className="card-img-top"
-                alt="Tshirt3"
+                src={Image.product3}
+                className="product-image"
+                alt="PTX Denim"
               />
-              <div className="card-body">
-                
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-            <div className="box">
-              <img
-                src={Image.image4}
-                className="card-img-top"
-                alt="Tshirt4"
-              />
-              <div className="card-body">
-               
+              <div className="product-info">
+                <h3 className="product-title">PTX Denim</h3>
+                <p className="product-description">
+                  Our collection of classic denim
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="container">
-        <div className="row mt-5">
-          <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-            <div className="box">
-              <img
-                src={Image.image5}
-                className="card-img-top"
-                alt="Tshirt1"
-              />
-              <div className="card-body">
+      <div className="space"></div>
+
+      <h2 className="text-center uppercase">
+      CURATED ESSENTIALS FOR A TIMELESS WARDROBE
+      </h2>
+      <p className="text-center">
+        Complement your everyday style with timeless essential items
+      </p>
+
+      <div className="container my-5">
+        <div className="row g-4">
+          {products.map((product) => (
+            <div
+              className="col-sm-12 col-md-6 col-lg-4 col-xl-3"
+              key={product.id}
+            >
+              <div className="product-card position-relative overflow-hidden rounded shadow-sm">
+                <div className="product-img-container position-relative">
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    className="img-fluid product-img"
+                  />
+                  <div className="product-overlay d-flex justify-content-center align-items-center">
+                    <button className="btn btn-dark btn-sm">
+                      View Details
+                    </button>
+                  </div>
+                </div>
+                <div className="p-3">
+                  <h6 className="mb-1">{product.name}</h6>
+                  <p className="text-muted mb-1">{product.price}</p>
+            
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-            <div className="box">
-              <img
-                src={Image.image6}
-                className="card-img-top"
-                alt="Tshirt2"
-              />
-              <div className="card-body">
-                
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-            <div className="box">
-              <img
-                src={Image.image7}
-                className="card-img-top"
-                alt="Tshirt3"
-              />
-              <div className="card-body">
-                
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-12 col-md-6 col-lg-4 col-xl-3">
-            <div className="box">
-              <img
-                src={Image.image8}
-                className="card-img-top"
-                alt="Tshirt4"
-              />
-              <div className="card-body">
-               
-              </div>
-            </div>
-          </div>
+          ))}
+          
         </div>
       </div>
-      <div className="Blog"></div>
-<div className="newsletter"></div>
 
-<br/>
+      <br />
+      
 
-   
+      <div className="space"></div>
+      <div className="container-fluid p-0">
+        <img src={Image.Fashion_Banner} alt="banner" className="banner" />
+      </div>
+
+<div className="space"> </div>
+
+<div className="container">
+  <div className="row text-center">
+    <div className="col-sm-4 col-lg-4 col-xl-4">
+      <div className="info-box">
+        <img src={Image.icon1} alt="icons" className="info-icon" />
+        <h5>SUPPORT 24/7</h5>
+        <p>Contact us 24 hours a day, 7 days a week</p>
+      </div>
+    </div>
+    <div className="col-sm-4 col-lg-4 col-xl-4">
+      <div className="info-box">
+        <img src={Image.icon2} alt="icons" className="info-icon" />
+        <h5>TRACK YOUR ORDER</h5>
+        <p>Click for the quick update on your order</p>
+      </div>
+    </div>
+    <div className="col-sm-4 col-lg-4 col-xl-4">
+      <div className="info-box">
+        <img src={Image.icon3} alt="icons" className="info-icon" />
+        <h5>RETURN & EXCHANGES</h5>
+        <p>Please view the return and exchange policy</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
     </>
   );
 }

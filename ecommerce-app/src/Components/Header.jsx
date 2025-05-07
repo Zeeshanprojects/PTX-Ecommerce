7b4f0d1bb754bee1ad08b0a225540cce16f5b4f1
Zeeshan Khan <126192035+Zeeshanprojects@users.noreply.github.com>
2025-05-07 16:07:47 +0500

    Full Code Updated

--- ecommerce-app/src/Components/Header.jsx
+++ ecommerce-app/src/Components/Header.jsx
@@ -1,82 +1,85 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Image from '../Images/Image'
-import './Header.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import Image from "../Images/Image";
+import "./Header.css";
 
 export default function Header() {
   return (
-   <>
- <div className='topbar'>
-    <div className='container'>
-   <p className='pt-3'> Free shipping, 30-day return or refund guarantee.</p> 
+    <>
+      <div className="topbar">
+        <div className="container">
+          <p className="pt-3">
+            {" "}
+            Free shipping, 30-day return or refund guarantee.
+          </p>
+        </div>
+      </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+        <div className="container">
+          <Link className="navbar-brand" to="#">
+            <img src={Image.logo} />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  HOME
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/Men">
+                  MEN
+                </Link>
+              </li>
 
-    </div>
- </div>
- <nav className="navbar navbar-expand-lg navbar-dark bg-black">
-  <div className="container">
-    <Link className="navbar-brand" to="#">
-      <img src={Image.logo}/>
-    </Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">
-            HOME
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link active" to="/Men">
-            MEN
-          </Link>
-        </li>
-      
-        <li className="nav-item">
-          <Link className="nav-link active" to="/Women">
-            WOMEN
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link active" to="/Kid">
-            KIDS
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link active" to="/Fleece">
-            FLEECE
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link active" to="/Shop">
-            CATALOG
-          </Link>
-        </li>
-      
-        <li className="nav-item">
-          <Link className="nav-link active" to="#">
-            LOOKBOOK
-          </Link>
-        </li>
-     
-     
-       
-      </ul>
-    
-    </div>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/Women">
+                  WOMEN
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/Kid">
+                  KIDS
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/Fleece">
+                  FLEECE
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/Shop">
+                  CATALOG
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link active" to="#">
+                  LOOKBOOK
+                </Link>
+              </li>
+            </ul>
+
+            <div className="d-flex align-items-center gap-3 ms-auto">
+    <img src={Image.user} alt="User Icon" className="header-icon " />
+    <img src={Image.cart} alt="Cart Icon" className="header-icon" />
   </div>
-</nav>
 
 
-   </>
-  )
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }

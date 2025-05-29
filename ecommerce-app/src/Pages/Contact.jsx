6e5ed2f21a7919cb6a3922343bd857921d4622ae
Zeeshan Khan <126192035+Zeeshanprojects@users.noreply.github.com>
2025-05-29 11:35:57 +0500

    code Updated

--- ecommerce-app/src/Pages/Contact.jsx
+++ ecommerce-app/src/Pages/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
 
     try {
       const response = await axios.post(
-  `${import.meta.env.VITE_API_URL}/EcommerceContact`,
+  `${import.meta.env.VITE_API_URL}/api/EcommerceContact`,
   formData
 );
       if (response.data.success) {
